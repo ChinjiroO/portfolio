58cae97241a1b1e689a35d827f0dd098409b3d6f
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-17 03:46:19 +0700

    Add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./componetns/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        midnight: { 100: "#A7AAAE", 200: "#24292E", 300: "#1F2428" },
+      },
+    },
   },
   plugins: [],
 };
