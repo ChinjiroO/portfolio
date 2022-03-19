83b50d478bd896dbbe66da2878e5b0b6252d09a4
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-19 21:18:32 +0700

    Update hero-section.

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -1,16 +1,15 @@
-import { motion } from "framer-motion";
+import { motion, useViewportScroll } from "framer-motion";
 
 export default function HeroSection() {
   return (
     <motion.div
-      initial={{ opacity: 0, x: -100 }}
+      initial={{ opacity: 0, x: -500 }}
       animate={{ opacity: 1, x: 0 }}
       transition={{ duration: 1.5 }}
-      whileInView={{ opacity: 1 }}
     >
       <div className="flex flex-col h-[95vh] w-full justify-center items-center font-bold max-w-5xl">
         <div className="flex text-md font-light">
-          <p className="mr-2 text-red-101">Hi,</p>
+          <p className="mr-2 text-red-101 animate-bounce">Hi!</p>
           <p className="">Welcome to my portfolio website</p>
         </div>
         <div className="text-3xl">
