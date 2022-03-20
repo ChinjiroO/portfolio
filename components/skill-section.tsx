03414fa7fe55f481265a00ac3cd5456878b7b204
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-21 00:58:31 +0700

    Update skill-section.tsx

--- components/skill-section.tsx
+++ components/skill-section.tsx
@@ -3,60 +3,96 @@ import Image from "next/image";
 
 const Skills = [
   {
-    id: 1,
     name: "html-5",
     src: "/assets/icons/logos_html-5.svg",
     group: "lang",
   },
-  { id: 2, name: "css", src: "/assets/icons/logos_css-3.svg", group: "lang" },
-  { id: 3, name: "dart", src: "/assets/icons/logos_dart.svg", group: "lang" },
+  { name: "css", src: "/assets/icons/logos_css-3.svg", group: "lang" },
+  { name: "dart", src: "/assets/icons/logos_dart.svg", group: "lang" },
   {
-    id: 4,
     name: "javascript",
     src: "/assets/icons/logos_javascript.svg",
     group: "lang",
   },
   {
-    id: 5,
     name: "typescript",
     src: "/assets/icons/logos_typescript.svg",
     group: "lang",
   },
-  { id: 6, name: "react", src: "/assets/icons/logos_react.svg", group: "fw" },
+  { name: "react", src: "/assets/icons/logos_react.svg", group: "fw" },
   {
-    id: 7,
+    name: "xamarin",
+    src: "/assets/icons/logos_xamarin.svg",
+    group: "fw",
+  },
+  {
     name: "flutter",
     src: "/assets/icons/logos_flutter.svg",
     group: "fw",
   },
   {
-    id: 8,
     name: "next.js",
     src: "/assets/icons/logos_nextjs.svg",
     group: "fw",
   },
   {
-    id: 9,
     name: "figma",
     src: "/assets/icons/logos_figma.svg",
     group: "etc",
   },
   {
-    id: 10,
     name: "postman",
     src: "/assets/icons/logos_postman.svg",
     group: "etc",
   },
   {
-    id: 11,
     name: "tailwind",
     src: "/assets/icons/logos_tailwindcss.svg",
     group: "etc",
   },
   {
-    id: 12,
     name: "mysql",
+    src: "/assets/icons/logos_mysql.svg",
+    group: "db",
+  },
+  {
+    name: "postgresql",
     src: "/assets/icons/logos_postgresql.svg",
+    group: "db",
+  },
+  {
+    name: "bootstrap",
+    src: "/assets/icons/logos_bootstrap.svg",
+    group: "etc",
+  },
+  {
+    name: "mongodb",
+    src: "/assets/icons/logos_mongodb.svg",
+    group: "db",
+  },
+  {
+    name: "c-sharp",
+    src: "/assets/icons/logos_c-sharp.svg",
+    group: "lang",
+  },
+  {
+    name: "mui",
+    src: "/assets/icons/logos_mui.svg",
+    group: "etc",
+  },
+  {
+    name: "nodejs",
+    src: "/assets/icons/logos_nodejs.svg",
+    group: "fw",
+  },
+  {
+    name: "vs-code",
+    src: "/assets/icons/logos_vs-code.svg",
+    group: "etc",
+  },
+  {
+    name: "github",
+    src: "/assets/icons/logos_github.svg",
     group: "etc",
   },
 ];
@@ -72,17 +108,13 @@ export default function SkillSection() {
     >
       <div className="w-full pb-5">
         <p className="text-3xl py-5">Skills</p>
-        <p className="text-base font-light">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget pulvinar
-          ut at amet, sed curabitur pulvinar purus amet.
-        </p>
       </div>
-      <div className="w-full">
-        <p className="pb-3">Langauges</p>
+      <div className="w-full px-10">
+        <p className="pb-3">● Langauges</p>
         <ul className="flex gap-4 flex-wrap">
-          {Skills.map((skill) =>
+          {Skills.map((skill, index) =>
             skill.group === "lang" ? (
-              <div key={skill.id}>
+              <div key={index}>
                 <li className="w-16 h-16">
                   <Image
                     src={skill.src}
@@ -96,11 +128,11 @@ export default function SkillSection() {
             ) : null
           )}
         </ul>
-        <p className="py-3">Frameworks</p>
+        <p className="py-3">● Frameworks</p>
         <ul className="flex gap-4 flex-wrap">
-          {Skills.map((skill) =>
+          {Skills.map((skill, index) =>
             skill.group === "fw" ? (
-              <div key={skill.id}>
+              <div key={index}>
                 <li className="w-16 h-16">
                   <Image
                     src={skill.src}
@@ -114,11 +146,29 @@ export default function SkillSection() {
             ) : null
           )}
         </ul>
-        <p className="py-3">Other</p>
+        <p className="py-3">● Databases</p>
         <ul className="flex gap-4 flex-wrap">
-          {Skills.map((skill) =>
+          {Skills.map((skill, index) =>
+            skill.group === "db" ? (
+              <div key={index}>
+                <li className="w-16 h-16">
+                  <Image
+                    src={skill.src}
+                    alt={skill.name}
+                    width={100}
+                    height={100}
+                    layout="responsive"
+                  />
+                </li>
+              </div>
+            ) : null
+          )}
+        </ul>
+        <p className="py-3">● Other</p>
+        <ul className="flex gap-4 flex-wrap">
+          {Skills.map((skill, index) =>
             skill.group === "etc" ? (
-              <div key={skill.id}>
+              <div key={index}>
                 <li className="w-16 h-16">
                   <Image
                     src={skill.src}
