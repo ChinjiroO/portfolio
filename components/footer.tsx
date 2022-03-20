b415ed4bf3d49e939c528257c2eca26b9c8cd006
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-21 01:17:52 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,24 +2,24 @@ import { BsLine, BsGithub, BsTwitter } from "react-icons/bs";
 
 export default function Footer() {
   return (
-    <div className="flex w-screen max-w-5xl justify-between items-center">
-      <ul className="flex">© 2022 GitHub, Inc.</ul>
+    <div className="flex flex-wrap w-full text-slate-200 max-w-5xl justify-between items-center px-4 py-4 sm:py-0 gap-4">
+      <ul className="flex">© 2022 Techin Seoikrathok.</ul>
       <div className="flex gap-4">
         <a
           href="https://line.me/ti/p/SEjsmdq3Yn"
-          className="flex items-center text-slate-200 hover:text-slate-300"
+          className="flex items-center hover:text-slate-300"
         >
           <BsLine className="text-xl" />
         </a>
         <a
           href="https://twitter.com/CeeJay8991"
-          className="flex items-center text-slate-200 hover:text-slate-300"
+          className="flex items-center hover:text-slate-300"
         >
           <BsTwitter className="text-xl" />
         </a>
         <a
           href="https://github.com/ChinjiroO"
-          className="flex items-center text-slate-200 hover:text-slate-300"
+          className="flex items-center hover:text-slate-300"
         >
           <BsGithub className="text-xl" />
         </a>
