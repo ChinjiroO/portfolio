aae01d734c5857291c12f38f19a4f8019cfe64ff
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-23 07:36:35 +0700

    Add objective-section.tsx

---
+++ components/objective-section.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function ObjectiveSection() {
+  return (
+    <div className="flex overflow-hidden flex-col h-[95vh] w-full justify-center items-center font-bold max-w-5xl py-10 px-5">
+      <div>
+        <p className="text-3xl py-5">Objective</p>
+        <p className="">
+          “Graduated with a B.Ed. in Computer Education and seeking an Front-End
+          Developer position. Skilled in web development, mobile application
+          development (Hybrid apps), and Rest API. Adept knowledge of HTML, CSS,
+          JavaScript, React.js, Responsive design, and web development.”
+        </p>
+      </div>
+    </div>
+  );
+}
