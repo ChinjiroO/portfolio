eebc0ed4677dbf7cfb6817bf3359de4089c761a5
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-31 03:25:45 +0700

    Update tailwind theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,8 @@ module.exports = {
         midnight: { 100: "#A7AAAE", 200: "#24292E", 300: "#1F2428" },
         red: { 101: "#E06C79" },
         blue: { 101: "#9ECAFE", 201: "#539bf5" },
+        green: { 101: "#7FDC96" },
+        purple: { 101: "#B392F0" },
       },
     },
   },
