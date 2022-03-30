c29a9838b52ecce607d0e3d6e94b1c3dd6eac36e
ChinjiroO <techinseoikrathok@gmail.com>
2022-03-30 20:05:42 +0700

    Update parallax.tsx

--- components/parallax.tsx
+++ components/parallax.tsx
@@ -1,11 +1,11 @@
 import { useState, useRef, ReactNode } from "react";
-import { useIsomorphicLayoutEffect } from "framer-motion";
 
 import {
   motion,
   useViewportScroll,
   useTransform,
   useSpring,
+  useIsomorphicLayoutEffect,
   useReducedMotion,
 } from "framer-motion";
 
@@ -22,30 +22,24 @@ const Parallax = ({ children, offset = 50 }: ParallaxProps): JSX.Element => {
   const [clientHeight, setClientHeight] = useState(0);
   const ref = useRef<HTMLInputElement>(null);
 
-  // start animating our element when we've scrolled it into view
   const initial = elementTop - clientHeight;
-  // end our animation when we've scrolled the offset specified
   const final = elementTop + offset;
 
-  // const y = useTransform(scrollY, [initial, final], [offset, -offset])
   const yRange = useTransform(scrollY, [initial, final], [offset, -offset]);
-  // apply a spring to ease the result
   const y = useSpring(yRange, { stiffness: 400, damping: 90 });
 
+  //* useIsomorphicLayoutEffect() instead of useLayoutEffect()
   useIsomorphicLayoutEffect(() => {
     const element = ref?.current?.getBoundingClientRect().top;
-    // save our layout measurements in a function in order to trigger
-    // it both on mount and on resize
     const onResize = () => {
-      // use getBoundingClientRect instead of offsetTop in order to
-      // get the offset relative to the viewport
       if (!element) {
-        console.log("error");
+        console.log("element === undefined ");
       } else {
         setElementTop(element + window.scrollY || window.pageYOffset);
       }
       setClientHeight(window.innerHeight);
     };
+
     onResize();
     window.addEventListener("resize", onResize);
     return () => window.removeEventListener("resize", onResize);
