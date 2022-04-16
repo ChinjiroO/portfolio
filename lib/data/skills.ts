c9616209f12fb2e7129f36c46b429d41835afa2d
ChinjiroO <techinseoikrathok@gmail.com>
2022-04-16 23:49:52 +0700

    update: skill infomation

--- lib/data/skills.ts
+++ lib/data/skills.ts
@@ -1,97 +1,97 @@
 const Skills = [
-	{
-		name: "html-5",
-		src: "/assets/icons/logos_html-5.svg",
-		group: "lang",
-	},
-	{ name: "css", src: "/assets/icons/logos_css-3.svg", group: "lang" },
-	{ name: "dart", src: "/assets/icons/logos_dart.svg", group: "lang" },
-	{
-		name: "javascript",
-		src: "/assets/icons/logos_javascript.svg",
-		group: "lang",
-	},
-	{
-		name: "typescript",
-		src: "/assets/icons/logos_typescript.svg",
-		group: "lang",
-	},
-	{ name: "react", src: "/assets/icons/logos_react.svg", group: "fw" },
-	{
-		name: "xamarin",
-		src: "/assets/icons/logos_xamarin.svg",
-		group: "fw",
-	},
-	{
-		name: "flutter",
-		src: "/assets/icons/logos_flutter.svg",
-		group: "fw",
-	},
-	{
-		name: "next.js",
-		src: "/assets/icons/logos_nextjs.svg",
-		group: "fw",
-	},
-	{
-		name: "figma",
-		src: "/assets/icons/logos_figma.svg",
-		group: "etc",
-	},
-	{
-		name: "postman",
-		src: "/assets/icons/logos_postman.svg",
-		group: "etc",
-	},
-	{
-		name: "tailwindcss",
-		src: "/assets/icons/logos_tailwindcss.svg",
-		group: "etc",
-	},
-	{
-		name: "mysql",
-		src: "/assets/icons/logos_mysql.svg",
-		group: "db",
-	},
-	{
-		name: "postgresql",
-		src: "/assets/icons/logos_postgresql.svg",
-		group: "db",
-	},
-	{
-		name: "bootstrap",
-		src: "/assets/icons/logos_bootstrap.svg",
-		group: "etc",
-	},
-	{
-		name: "mongodb",
-		src: "/assets/icons/logos_mongodb.svg",
-		group: "db",
-	},
-	{
-		name: "c-sharp",
-		src: "/assets/icons/logos_c-sharp.svg",
-		group: "lang",
-	},
-	{
-		name: "mui",
-		src: "/assets/icons/logos_mui.svg",
-		group: "etc",
-	},
-	{
-		name: "node.js",
-		src: "/assets/icons/logos_nodejs.svg",
-		group: "fw",
-	},
-	{
-		name: "vs-code",
-		src: "/assets/icons/logos_vs-code.svg",
-		group: "etc",
-	},
-	{
-		name: "github",
-		src: "/assets/icons/logos_github.svg",
-		group: "etc",
-	},
+  {
+    name: "html-5",
+    src: "/assets/icons/logos_html-5.svg",
+    group: "lang",
+  },
+  { name: "css", src: "/assets/icons/logos_css-3.svg", group: "lang" },
+  // { name: "dart", src: "/assets/icons/logos_dart.svg", group: "lang" },
+  {
+    name: "javascript",
+    src: "/assets/icons/logos_javascript.svg",
+    group: "lang",
+  },
+  {
+    name: "typescript",
+    src: "/assets/icons/logos_typescript.svg",
+    group: "lang",
+  },
+  { name: "react", src: "/assets/icons/logos_react.svg", group: "fw" },
+  {
+    name: "xamarin",
+    src: "/assets/icons/logos_xamarin.svg",
+    group: "fw",
+  },
+  // {
+  // 	name: "flutter",
+  // 	src: "/assets/icons/logos_flutter.svg",
+  // 	group: "fw",
+  // },
+  {
+    name: "next.js",
+    src: "/assets/icons/logos_nextjs.svg",
+    group: "fw",
+  },
+  {
+    name: "figma",
+    src: "/assets/icons/logos_figma.svg",
+    group: "etc",
+  },
+  {
+    name: "postman",
+    src: "/assets/icons/logos_postman.svg",
+    group: "etc",
+  },
+  {
+    name: "tailwindcss",
+    src: "/assets/icons/logos_tailwindcss.svg",
+    group: "etc",
+  },
+  // {
+  // 	name: "mysql",
+  // 	src: "/assets/icons/logos_mysql.svg",
+  // 	group: "db",
+  // },
+  {
+    name: "postgresql",
+    src: "/assets/icons/logos_postgresql.svg",
+    group: "db",
+  },
+  {
+    name: "bootstrap",
+    src: "/assets/icons/logos_bootstrap.svg",
+    group: "etc",
+  },
+  {
+    name: "mongodb",
+    src: "/assets/icons/logos_mongodb.svg",
+    group: "db",
+  },
+  {
+    name: "c-sharp",
+    src: "/assets/icons/logos_c-sharp.svg",
+    group: "lang",
+  },
+  {
+    name: "mui",
+    src: "/assets/icons/logos_mui.svg",
+    group: "etc",
+  },
+  {
+    name: "node.js",
+    src: "/assets/icons/logos_nodejs.svg",
+    group: "fw",
+  },
+  {
+    name: "vs-code",
+    src: "/assets/icons/logos_vs-code.svg",
+    group: "etc",
+  },
+  {
+    name: "github",
+    src: "/assets/icons/logos_github.svg",
+    group: "etc",
+  },
 ];
 
 export default Skills;
